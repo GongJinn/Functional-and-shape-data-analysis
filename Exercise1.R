8111e934d1adc02433d5f6f429dd86a1c6317be3
GongJinn <37574886+GongJinn@users.noreply.github.com>
2019-01-16 00:10:45 +0200

    Ex1 part2

---
+++ Exercise1.R
@@ -0,0 +1,41 @@
+library(fda)
+#1.a
+data(pinch)
+
+bspline15<-create.bspline.basis(c(1,151),nbasis=15, norder=4)
+
+basismatrix<-eval.basis((1:151), bspline15)
+coef.pinch<-lsfit(basismatrix,pinch, intercept=FALSE)$coef
+t<-seq(1,151, 0.1)
+
+basismatrix1<-eval.basis(t, bspline15)
+plot((1:151), pinch[,1],type="n",xlab="x",ylab="y")#empty plot
+for(i in c(1:20)){
+  smooth.pinch<-basismatrix1%*%coef.pinch[,i]
+  lines(t,smooth.pinch)
+}
+#1.b
+mean.pinch<-rowMeans(pinch)
+mean.coef.pinch<-lsfit(basismatrix,mean.pinch, intercept=FALSE)$coef
+smooth.pinch<-basismatrix1%*%mean.coef.pinch
+lines(t,smooth.pinch, col="red")
+#1.c
+H.matrix<-basismatrix%*%solve(t(basismatrix)%*%basismatrix)%*%t(basismatrix)
+fitted.pinch<-H.matrix%*%pinch
+SSE.pinch<-colSums((fitted.pinch-pinch)^2)
+#2.a
+List.pinch<-smooth.basis((1:151),pinch,bspline15)
+#2.b
+derivatives.first<-deriv.fd(List.pinch$fd,1)
+plot(derivatives.first)
+derivatives.second<-deriv.fd(List.pinch$fd,2)
+plot(derivatives.second)
+#2.c
+eval.pinch <- eval.fd((1:151),List.pinch$fd,1)
+plot(derivatives.first[1])
+eval.pinch1 <- eval.fd((1:151),derivatives.first[1])
+for(i in c(1:150)){
+  if (eval.pinch1[i]*eval.pinch1[i+1]<=0){
+    print(i+0.5)
+  }
+}
